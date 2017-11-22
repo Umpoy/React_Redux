15d290fda28f1805f39b07895f777f72d7b1d30d
Umpoy <iumpoy@yahoo.com>
2017-11-22 12:14:25 -0800

    Comiting before introduction to middleware

---
+++ src/actions/index.js
@@ -0,0 +1,1 @@
+const API_KEY = '57c030306109ae28ab5c54130e3db172';
---
+++ src/containers/search_bar.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { term: '' }
+        this.onInputChange = this.onInputChange.bind(this)
+    }
+
+    onInputChange(event) {
+        this.setState({ term: event.target.value })
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault()
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onFormSubmit} className="input-group">
+                <input type="text" className="form-control" placeholder="Get a five-day forecast in your favorite cities" value={this.state.form} onChange={this.onInputChange} />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
+            </form>
+        )
+    }
+}
